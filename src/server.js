0d19c2c19040ccabb1bcacbf353bb3ff3544f9b7
GuiQRSM <quaresmagui20@gmail.com>
2025-07-31 11:27:02 -0300

    project bulding

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import http from 'node:http'
+
+const users = []
+
+const server = http.createServer((req, res) => {
+
+    const { method, url } = req
+
+    if(method === 'GET' && url === '/users') {
+      
+        
+        return res
+        .setHeader('Content-type', 'aplication/json')
+        .end(JSON.stringify(users))
+
+    }
+
+    if(method === 'POST' && url === '/users' ) {
+        
+        users.push({
+            id: 1,
+            name: 'Carlos Alberto',
+            email: 'albertocarlos@gmail.com',
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
+})
+
+server.listen(3333, () => {
+    console.log('Servidor funcional')
+})
+
+// localhost: 3333;
+// -Dentro desse req eu consigo obter todas as informações da requisição que chega no servidor;
+// -res vai ser utilizado pra devolver uma resposta pra quem está chamando;
