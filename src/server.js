fd5447a3c131a175df7211adca29196e7bb77bb9
GuiQRSM <quaresmagui20@gmail.com>
2025-10-16 11:09:43 -0300

    add: create middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import http from 'node:http';
+import { json } from '../middlewares/json.js';
 
 const users = [];
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === 'GET' && url === '/users') {
     return res.setHeader('Content-type', 'aplication/json').end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
       name,
