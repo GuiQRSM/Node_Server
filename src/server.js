74d2885b1b18997d57cf53b50f64312cfa57aae4
GuiQRSM <quaresmagui20@gmail.com>
2025-10-16 09:26:15 -0300

    add: reads the body of the request

--- src/server.js
+++ src/server.js
@@ -2,9 +2,22 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const { method, url } = req
+
+        const buffers = [];
+  for await (const chunck of req) {
+    buffers.push(chunck)
+  }
+
+  try{
+    req.body =  JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  console.log(req.body)
 
     if(method === 'GET' && url === '/users') {
       
@@ -16,11 +29,13 @@ const server = http.createServer((req, res) => {
     }
 
     if(method === 'POST' && url === '/users' ) {
+
+        const {name, email} = req.body
         
         users.push({
             id: 1,
-            name: 'Carlos Alberto',
-            email: 'albertocarlos@gmail.com',
+            name,
+            email,
         })
 
         return res.writeHead(201).end()
