ab7a90e25b9163a643779f1ff1742db75adb5a46
GuiQRSM <quaresmagui20@gmail.com>
2025-10-21 10:09:11 -0300

    add: create const pathRegex

--- utils/build-route-path.js
+++ utils/build-route-path.js
@@ -2,4 +2,6 @@ export function buildRouterPath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
 
   const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9-_]+)');
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
 }
