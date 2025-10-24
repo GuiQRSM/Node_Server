5c499a1962b9dec0ebfe80d90d59f392c37d2724
GuiQRSM <quaresmagui20@gmail.com>
2025-10-23 21:45:19 -0300

    add: first part of the reduce

--- utils/extract-query-params.js
+++ utils/extract-query-params.js
@@ -1,1 +1,14 @@
-export function extractQueryParams() {}
+export function extractQueryParams(query) {
+  //substr removes the first character '?'
+  //split creates an array separating by '&'
+  return query
+    .substr(1)
+    .split('&')
+    .reduce((queryParams, param) => {
+      const [key, value] = param.split('=');
+
+      queryParams[key] = value;
+
+      return queryParams;
+    }, {});
+}
