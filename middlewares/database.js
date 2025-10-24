be3a6e0bdfee7c4c9c52b181befdcc95a743e30e
GuiQRSM <quaresmagui20@gmail.com>
2025-10-24 10:17:08 -0300

    add: implements new parameter in the 'select' function and implements conditional for it, makes the const a let as well

--- middlewares/database.js
+++ middlewares/database.js
@@ -17,8 +17,12 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
-  select(table) {
-    const data = this.#database[table] ?? [];
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter();
+    }
 
     return data;
   }
